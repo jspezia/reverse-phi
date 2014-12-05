ce78d5963c4ffb28ab6634145d68e3c0901cfeff
jspezia <jspezia@student.42.fr>
2014-12-05 15:55:32 +0100

    b_sf 0.1, 0.66, 0.33

--- src/act.py
+++ src/act.py
@@ -42,9 +42,9 @@ def creation_stimulus(info, screen, param, name_database='blackwhite'):
         if (param.condition == 7):
             t, b, B_sf = np.pi/2, np.pi/32, 0.1
         if (param.condition == 8):
-            t, b, B_sf = 0, np.pi/32, 0.05
+            t, b, B_sf = 0, np.pi/32, 0.033
         if (param.condition == 9):
-            t, b, B_sf = 0, np.pi/32, 0.5
+            t, b, B_sf = 0, np.pi/32, 0.066
         fx, fy, ft = mc.get_grids(info[NS_X], info[NS_Y], 1)
         cloud = mc.random_cloud(mc.envelope_gabor(fx, fy, ft, B_sf=B_sf, theta=t, B_theta=b, B_V=1000.))
         cloud = rectif(cloud, contrast=1.)
